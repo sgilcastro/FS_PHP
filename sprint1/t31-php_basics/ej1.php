2a515b29705ef208f68c5034fcd74576ec9b193a
sgilcastro <susanagilcastro@gmail.com>
2022-11-14 13:27:03 +0100

    Haciendo pruebas de php

---
+++ sprint1/t31-php_basics/ej1.php
@@ -0,0 +1,14 @@
+<?PHP
+$nombre = "Susana";
+$edad = 48;
+$altura = 1.64;
+$paro = "false";
+
+echo "<p>Exercici 1. Defineix una variable de cada tipus: integer,double,string i boolean. Després, imprimeix-les per pantalla.</p>";
+
+echo "Mi nombre es " 
+. $nombre . ". Mido " 
+. $altura . " mts. Tengo " 
+. $edad . " años. Estoy en paro: "
+. $paro;
+?>
