3ddb417fe0e78d66f81fbe7be8b8ebf5c5d35f6c
sgilcastro <susanagilcastro@gmail.com>
2022-11-15 11:23:42 +0100

    Ejercicio 1

--- sprint1/t31-php_basics/ej1.php
+++ sprint1/t31-php_basics/ej1.php
@@ -1,14 +1,47 @@
-<?PHP
-$nombre = "Susana";
-$edad = 48;
-$altura = 1.64;
-$paro = "false";
+<!DOCTYPE html>
 
-echo "<p>Exercici 1. Defineix una variable de cada tipus: integer,double,string i boolean. Després, imprimeix-les per pantalla.</p>";
+<html lang="es">
 
-echo "Mi nombre es " 
-. $nombre . ". Mido " 
-. $altura . " mts. Tengo " 
-. $edad . " años. Estoy en paro: "
-. $paro;
-?>
+    <head>
+        <title>Sprint 1. Php basicos (Tema 3).</title>
+        <meta charset="utf-8">
+        <meta name="description" content="Entrega d'exercici: Php basics (Tema 3). Sprint 1. Nivel 1. ejercicios 1, 2, 3, 4 y 5">
+        <meta name="author" content="Susana Gil">
+        <meta name="keywords" content="php html5">
+        <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1"> <!-- Este meta se usa para la visualización en pantallas móviles-->
+        
+        <!-- <meta name="robots" content="no-index"> esta linea sólo se se usa si no quieres indexar. Por defecto indexa siempre -->
+        
+        <!-- css reset -->
+        <link rel="stylesheet" type="text/css" href="css/normalize.css"> 
+
+        <!-- css mi estilo -->
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+    </head>
+
+
+    <body>
+        <?PHP
+
+        /* Variables ejercicio1 */
+        $nombre = "Susana";
+        $edad = 48;
+        $altura = 1.64;
+        $paro = "false";
+
+       /*Ejercicios*/
+
+        echo "<h1>Exercici 1</h1>";
+        /* Defineix una variable de cada tipus: integer,double,string i boolean. Després, imprimeix-les per pantalla.*/
+        echo "Mi nombre es "
+        . $nombre . ". Mido "
+        . $altura . " mts. Tengo "
+        . $edad . " años. Estoy en paro: "
+        . $paro;
+        
+        ?>
+
+    </body>
+
+</html>
