612bf1c9805611eb10f345316ba0ef30ea6aa647
sgilcastro <susanagilcastro@gmail.com>
2022-11-15 12:18:05 +0100

    Enunciats Nivel1: Funcions i Estructures (T3)

---
+++ sprint1/t32-funcions_i_estructures_de_control/nivel1.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+    <head>
+        <title>Funcions i Estructures de control (Tema 3).</title>
+        <meta charset="utf-8">
+        <meta name="description" content=
+        "Entrega d'exercici: Funcions i Estructures de control (Tema 3). Sprint 1. Nivel 1.
+        Ejercicios 1, 2, 3, 4, 5 y 6">
+        <meta name="author" content="Susana Gil">
+        <meta name="keywords" content="php html5 funciones controldeflujo">
+        <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1">
+        <!-- Este meta se usa para la visualización en pantallas móviles-->
+
+    <body>
+        <?PHP
+
+        /* Variables ejercicio1 */
+        
+        /* Variables ejercicio2 */
+
+        /* Variables ejercicio3 */
+
+        /* Variables ejercicio4 */
+
+        /* Variables ejercicio5 */
+
+        /* Variables ejercicio6 */
+        
+        /*Ejercicios*/
+
+        echo "<h1>Exercici 1</h1>";
+        /* Programa una funció que, donat un número qualsevol (per exemple, la teva edat)
+        ens digui si és parell o imparell mitjançant un missatge per pantalla.*/
+
+        echo "<h1>Exercici 2</h1>";
+        /* Per jugar a l'"amagatall" se'ns ha demanat un programa que compti fins a 10.
+        Però la persona que comptarà és una mica tramposa i ho farà comptant de dos en dos.
+        Crea una funció que compti fins a 10, de 2 en 2, mostrant cada número del compte per pantalla.*/
+
+        echo "<h1>Exercici 3</h1>";
+        /* Imagina't que volem que compti fins a un nombre diferent de 10.
+        Programa la funció perquè el final del compte estigui parametritzat.*/
+
+        echo "<h1>Exercici 4</h1>";
+        /* Per prevenir oblits en utilitzar la nostra meravellosa opció "amagatall" establirem un
+         paràmetre per defecte igual a 10 a la funció que s'encarrega de fer aquest compte.*/
+
+        echo "<h1>Exercici 5</h1>";
+        /* Escriure una funció per verificar el grau d'un/a estudiant d'acord amb la nota.
+        Condicions:
+        Si la nota és 60% o més, el grau hauria de ser Primera Divisió.
+        Si la nota està entre 45% i 59%, el grau hauria de ser Segona Divisió.
+        Si la nota està entre 33% to 44%, el grau hauria de ser Tercera Divisió.
+        Si la nota és menor a 33%, l'estudiant reprovarà.*/
+
+        echo "<h1>Exercici 6</h1>";
+        /*Charlie em va mossegar el dit!
+        Charlie et mossegarà el dit exactament el 50% del temps.
+        Escriu La funció isBitten () que retorna TRUE amb un 50% de probabilitat i FALSE en cas contrari.
+        Important
+        Consell: pot ser que la funció rand () et resulti útil. */
+
+        ?>
+
+    </body>
+
+</html>
