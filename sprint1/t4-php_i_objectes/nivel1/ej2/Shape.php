5861d490b93e53db30c465cf0448ff1d8924d6c0
sgilcastro <susanagilcastro@gmail.com>
2022-11-21 13:34:37 +0100

    T4. N1. Ej2

--- sprint1/t4-php_i_objectes/nivel1/ej2/Shape.php
+++ sprint1/t4-php_i_objectes/nivel1/ej2/Shape.php
@@ -1,25 +1,33 @@
-<!DOCTYPE html>
-
-<html lang="es">
-
-    <head>
-        <title>PHP y objetos Objects (Tema 4).</title>
-        <meta charset="utf-8">
-        <meta name="description" content=
-        "Entrega d'exercici: PHP i Objects (Tema 4). Sprint 1. Nivel 1.
-        Ejercicios 1">
-        <meta name="author" content="Susana Gil">
-        <meta name="keywords" content="php html5 objetos">
-        <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1">
-        <!-- Este meta se usa para la visualización en pantallas móviles-->
-
-    </head>
-
-    <body>
-
     <?php
-        
-    ?>
+        class Shape {
+            //Atributos de clase
+            protected $ancho;
+            protected $alto;
+            
 
-    </body>
-</html>
+            public function __construct ($ancho, $alto) {
+                $this->ancho=$ancho;
+                $this->alto=$alto;
+            }
+
+
+        }
+
+        //Clase hija
+        class Triangulo extends Shape {
+        /*El área o superficie de un triángulo cualquiera es igual
+        al producto de la base por la altura dividido por dos.*/
+            public function area() {
+                return $this->area = ($this->ancho*$this->alto)/2;
+            }
+        }
+
+        //Clase hija 
+        class Rectangulo extends Shape {
+        /*Para calcular el área de un rectángulo multiplicamos
+        el largo por el ancho.*/
+            public function area(){
+                return $this->area = $this->ancho*$this->alto;
+            }
+        }
+    ?>
--- sprint1/t4-php_i_objectes/nivel1/ej2/shapes.php
+++ sprint1/t4-php_i_objectes/nivel1/ej2/shapes.php
@@ -1,3 +1,35 @@
-<?php
+<!DOCTYPE html>
 
-?>
+<html lang="es">
+
+    <head>
+        <title>PHP y objetos Objects (Tema 4).</title>
+        <meta charset="utf-8">
+        <meta name="description" content=
+        "Entrega d'exercici: PHP i Objects (Tema 4). Sprint 1. Nivel 1.
+        Ejercicios 1">
+        <meta name="author" content="Susana Gil">
+        <meta name="keywords" content="php html5 objetos">
+        <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1">
+        <!-- Este meta se usa para la visualización en pantallas móviles-->
+
+    </head>
+
+    <body>
+
+        <?php
+            include("Shape.php");
+
+            $s1 = new Triangulo(2, 4);
+            echo "El area del triangulo es: " . $s1->area();
+            echo '<br>';
+
+            $s2 = new Rectangulo(2, 4);
+            echo "El area del rectangulo es: " . $s2->area();
+            echo '<br>';
+
+        ?>
+
+
+    </body>
+</html>
