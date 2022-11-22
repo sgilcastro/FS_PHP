00e53ebb850176354a8d2c6f0c994bab9a1c92c1
sgilcastro <susanagilcastro@gmail.com>
2022-11-22 12:19:17 +0100

    Sprint1 - t4 N1 preparacion

---
+++ sprint1/t4-php_i_objectes/nivel2/PokerDice.php
@@ -0,0 +1,42 @@
+<?php
+
+    class Shape {
+
+        //Atributos de clase
+        protected $figura;
+
+
+        //Metodos
+        throw(){
+
+            $figura=mt_rand(1, 6);
+            
+            echo $d;
+            echo "<br>";
+            
+            switch ($figura) {
+                case 1:
+                    $mordedura = true;
+                    break;
+                case 2:
+                    $mordedura = false;
+                    break;
+                case 3:
+                    $mordedura = false;
+                    break;
+                case 4:
+                    $mordedura = false;
+                    break;
+                case 5:
+                    $mordedura = false;
+                    break;
+                case 6:
+
+            
+
+            }
+
+        }
+        
+    }
+?>
