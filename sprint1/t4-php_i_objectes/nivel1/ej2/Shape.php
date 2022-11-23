f52273e83b84a36e1746d0bfe44b5182d30dc202
sgilcastro <susanagilcastro@gmail.com>
2022-11-23 09:37:22 +0100

    correcciones menores

--- sprint1/t4-php_i_objectes/nivel1/ej2/Shape.php
+++ sprint1/t4-php_i_objectes/nivel1/ej2/Shape.php
@@ -1,33 +1,37 @@
-    <?php
-        class Shape {
-            //Atributos de clase
-            protected $ancho;
-            protected $alto;
-            
-
-            public function __construct ($ancho, $alto) {
-                $this->ancho=$ancho;
-                $this->alto=$alto;
-            }
+<?php
+	class Shape
+	{
+		//Atributos de clase
+		protected $ancho;
+		protected $alto;
 
 
-        }
+		public function __construct($ancho, $alto)
+		{
+			$this->ancho = $ancho;
+			$this->alto = $alto;
+		}
+	}
 
-        //Clase hija
-        class Triangulo extends Shape {
-        /*El área o superficie de un triángulo cualquiera es igual
-        al producto de la base por la altura dividido por dos.*/
-            public function area() {
-                return $this->area = ($this->ancho*$this->alto)/2;
-            }
-        }
+	//Clase hija
+	class Triangulo extends Shape
+	{
+		/*El área o superficie de un triángulo cualquiera es igual
+		al producto de la base por la altura dividido por dos.*/
+		public function area()
+		{
+			return $this->area = ($this->ancho * $this->alto) / 2;
+		}
+	}
 
-        //Clase hija 
-        class Rectangulo extends Shape {
-        /*Para calcular el área de un rectángulo multiplicamos
-        el largo por el ancho.*/
-            public function area(){
-                return $this->area = $this->ancho*$this->alto;
-            }
-        }
-    ?>
+	//Clase hija
+	class Rectangulo extends Shape
+	{
+		/*Para calcular el área de un rectángulo multiplicamos
+		el largo por el ancho.*/
+		public function area()
+		{
+			return $this->area = $this->ancho * $this->alto;
+		}
+	}
+?>
