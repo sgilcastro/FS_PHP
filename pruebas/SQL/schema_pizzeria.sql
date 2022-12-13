c562a2468367b0c3ad4c7b687f21f9c4e08bf52a
sgilcastro <susanagilcastro@gmail.com>
2022-12-13 11:19:49 +0100

    Modificada estructura de carpetas

--- pruebas/SQL/schema_pizzeria.sql
+++ pruebas/SQL/schema_pizzeria.sql
@@ -2,6 +2,14 @@ DROP DATABASE IF EXISTS pizzeria;
 CREATE DATABASE pizzeria CHARACTER SET utf8mb4;
 USE pizzeria;
 
+CREATE TABLE tienda (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    direccion VARCHAR(50) NOT NULL,
+    codigo_postal VARCHAR(5) NOT NULL,
+    localidad VARCHAR(25) NOT NULL,
+    provincia VARCHAR(25) NOT NULL
+);
+
 CREATE TABLE cliente (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(25) NOT NULL,
@@ -13,19 +21,19 @@ CREATE TABLE cliente (
     telefono INT(9) UNIQUE 
 );
 
--- mirar esto https://learn.microsoft.com/es-es/sql/t-sql/statements/create-table-transact-sql-identity-property?view=sql-server-ver16
-
 CREATE TABLE pedido (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    fecha_hora DATETIME DEFAULT NOW() NOT NULL
-    id_cliente INT UNSIGNED NOT NULL UNIQUE,
+    fecha_hora DATETIME DEFAULT NOW() NOT NULL,
+    id_cliente INT UNSIGNED NOT NULL,
     tipo ENUM('local', 'domicilio') NOT NULL,
-    FOREIGN KEY(id_cliente) REFERENCES cliente(id)
+    id_tienda INT UNSIGNED NOT NULL,
+    FOREIGN KEY(id_cliente) REFERENCES cliente(id),
+    FOREIGN KEY(id_tienda) REFERENCES tienda(id)
 );
 
 CREATE TABLE producto (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(25) NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
     descripcion VARCHAR(225) NOT NULL,
     img VARCHAR(225) DEFAULT 'imagen_no_disponible.jpg' NOT NULL,
     precio FLOAT(6,2) NOT NULL,
@@ -35,7 +43,7 @@ CREATE TABLE producto (
 CREATE TABLE detalle_pedido (
     id_pedido INT UNSIGNED NOT NULL,
     id_producto INT UNSIGNED NOT NULL,
-    --unidades_producto INT UNSIGNED DEFAULT 1 NOT NULL,
+    unidades_producto INT UNSIGNED DEFAULT 1 NOT NULL,
     PRIMARY KEY (id_pedido, id_producto),
     FOREIGN KEY(id_pedido) REFERENCES pedido(id),
     FOREIGN KEY(id_producto) REFERENCES producto(id)
@@ -47,18 +55,10 @@ CREATE TABLE categoria (
 );
 
 CREATE TABLE categoria_producto_pizza (
-    id_pizza INT UNSIGNED PRIMARY KEY,
-    id_categoria INT UNSIGNED NOT NULL,
+    id_categoria INT UNSIGNED,
+    id_pizza INT UNSIGNED NOT NULL PRIMARY KEY,
     FOREIGN KEY(id_pizza) REFERENCES producto(id),
     FOREIGN KEY(id_categoria) REFERENCES categoria(id)
-);
-
-CREATE TABLE tienda (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    direccion VARCHAR(50) NOT NULL,
-    codigo_postal VARCHAR(5) NOT NULL,
-    localidad VARCHAR(25) NOT NULL,
-    provincia VARCHAR(25) NOT NULL
 );
 
 CREATE TABLE empleado (
@@ -68,20 +68,14 @@ CREATE TABLE empleado (
     NIF VARCHAR(10) NOT NULL,
     telefono VARCHAR(9), 
     puesto ENUM('repartidor', 'cocinero') NOT NULL,
-    id_tienda INT UNSIGNED NOT NULL
-);
-
-CREATE TABLE detalle_empleados_tienda (
-    id_tienda INT UNSIGNED PRIMARY KEY,
-    id_empleado INT UNSIGNED NOT NULL,
-    FOREIGN KEY(id_tienda) REFERENCES tienda(id),
-    FOREIGN KEY(id_empleado) REFERENCES empleado(id)
+    id_tienda INT UNSIGNED NOT NULL,
+    FOREIGN KEY(id_tienda) REFERENCES tienda(id)
 );
 
 CREATE TABLE pedido_a_domicilio (
     id_pedido INT UNSIGNED PRIMARY KEY,
     id_empleado INT UNSIGNED NOT NULL,
-    fecha_hora_entrega DATETIME DEFAULT NOW() NOT NULL
+    fecha_hora_entrega DATETIME DEFAULT NOW() NOT NULL,
     FOREIGN KEY(id_pedido) REFERENCES pedido(id),
     FOREIGN KEY(id_empleado) REFERENCES empleado(id)
 );
@@ -102,66 +96,54 @@ INSERT INTO cliente (nombre, apellidos, direccion, codigo_postal, localidad, pro
 INSERT INTO cliente (nombre, apellidos, direccion, codigo_postal, localidad, provincia, telefono) VALUES ('Mario', 'Santos', 'Calle Mallorca 98', '08007', 'Barcelona', 'Barcelona', '789123654');
 
 /* Pedido */
-INSERT INTO pedido (id, fecha_hora, id_cliente, tipo) VALUES (1, '2022/05/01 01:00', 1, 'local');
-INSERT INTO pedido (fecha_hora, id_cliente, tipo) VALUES ('2022-12-12 12:00:00', 1, 'local');
-INSERT INTO pedido (fecha_hora, id_cliente, tipo) VALUES ('2022-12-12 12:30:00', 2, 'domicilio');
-INSERT INTO pedido (fecha_hora, id_cliente, tipo) VALUES ('2022-12-12 13:00:00', 3, 'local');
-INSERT INTO pedido (fecha_hora, id_cliente, tipo) VALUES ('2022-12-12 13:30:00', 4, 'domicilio');
+INSERT INTO pedido (id, fecha_hora, id_cliente, tipo, id_tienda) VALUES (1, '2022/05/01 01:00', 1, 'local', 2);
+INSERT INTO pedido (fecha_hora, id_cliente, tipo, id_tienda) VALUES ('2022-12-12 12:00:00', 1, 'local', 2);
+INSERT INTO pedido (fecha_hora, id_cliente, tipo, id_tienda) VALUES ('2022-12-12 12:30:00', 2, 'domicilio', 1);
+INSERT INTO pedido (fecha_hora, id_cliente, tipo, id_tienda) VALUES ('2022-12-12 13:00:00', 3, 'local', 2);
+INSERT INTO pedido (fecha_hora, id_cliente, tipo, id_tienda) VALUES ('2022-12-12 13:30:00', 4, 'domicilio', 1);
 
 /* Producto */
-CREATE TABLE producto (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(25) NOT NULL,
-    descripcion VARCHAR(225) NOT NULL,
-    img VARCHAR(225) DEFAULT 'imagen_no_disponible.jpg' NOT NULL,
-    precio FLOAT(6,2) NOT NULL,
-    tipo ENUM('pizza', 'hamburguesa', 'bebida') NOT NULL
-);
+
 INSERT INTO producto VALUES (1, 'Pizza margarita', 'Pizza con queso y tomate', '000001.jpg', '9', 'pizza');
 INSERT INTO producto (nombre, descripcion, img, precio, tipo) VALUES ('Hamburguesa clásica', 'Hamburguesa con carne, queso, lechuga, tomate y cebolla', 'hamburguesa_clasica.jpg', 8.50, 'hamburguesa');
 INSERT INTO producto (nombre, descripcion, img, precio, tipo) VALUES ('Coca-Cola', 'Bebida gaseosa de cola', 'cocacola.jpg', 2.00, 'bebida');
-INSERT INTO producto (nombre, descripcion, img, precio, tipo) VALUES ('Pizza pepperoni', 'Pizza con pepperoni y queso', 'pizza_pepperoni.jpg', 11.50, 'pizza');
+INSERT INTO producto (nombre, descripcion, img, precio, tipo) VALUES ('Pizza de chocolate', 'Pizza con chocolate y almendras', 'pizza_chocolate.jpg', 11.50, 'pizza');
 INSERT INTO producto (nombre, descripcion, img, precio, tipo) VALUES ('Hamburguesa de pollo', 'Hamburguesa con carne de pollo, queso, lechuga, tomate y cebolla', 'hamburguesa_pollo.jpg', 9.00, 'hamburguesa');
 INSERT INTO producto (nombre, descripcion, img, precio, tipo) VALUES ('Agua mineral', 'Bebida sin gas', 'agua_mineral.jpg', 1.50, 'bebida');
 INSERT INTO producto (nombre, descripcion, img, precio, tipo) VALUES ('Pizza de champiñones', 'Pizza con champiñones y queso', 'pizza_champinones.jpg', 12.00, 'pizza');
 INSERT INTO producto (nombre, descripcion, img, precio, tipo) VALUES ('Pepsi', 'Bebida gaseosa de cola', 'pepsi.jpg', 2.00, 'bebida');
-INSERT INTO producto (nombre, descripcion, img, precio, tipo) VALUES ('Pizza de pepperoni y champiñones', 'Pizza con pepperoni, champiñones y queso', 'pizza_pepperoni_champinones.jpg', 14.00, 'pizza');
+INSERT INTO producto (nombre, descripcion, img, precio, tipo) VALUES ('Pizza de pepperoni', 'Pizza con pepperoni, champiñones y queso', 'pizza_pepperoni_champinones.jpg', 14.00, 'pizza');
 INSERT INTO producto (nombre, descripcion, img, precio, tipo) VALUES ('Hamburguesa vegetariana', 'Hamburguesa con verduras y queso', 'hamburguesa_vegetariana.jpg', 7.50, 'hamburguesa');
 INSERT INTO producto (nombre, descripcion, img, precio, tipo) VALUES ('Agua con gas', 'Bebida con gas', 'agua_con_gas.jpg', 1.75, 'bebida');
 
 
 /* Detalle pedido */
 
-INSERT INTO detalle_pedido VALUES (1, 5);
-INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (1, 5);
-INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (1, 5);
-INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (1, 7);
-INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (1, 3);
+INSERT INTO detalle_pedido VALUES (1, 5, 2);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (1, 6);
+INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (1, 7);
+INSERT INTO detalle_pedido (id_pedido, id_producto, unidades_producto) VALUES (1, 3, 3);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (1, 2);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (1, 8);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (1, 1);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (1, 10);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (1, 9);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (1, 4);
-INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (2, 7);
+INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (2, 10);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (2, 5);
-INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (2, 7);
+INSERT INTO detalle_pedido (id_pedido, id_producto, unidades_producto)  VALUES (2, 7, 2);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (2, 3);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (2, 6);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (2, 2);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (3, 3);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (3, 5);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (3, 7);
-INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (3, 3);
+INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (3, 8);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (3, 6);
-INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (4, 6);
+INSERT INTO detalle_pedido (id_pedido, id_producto, unidades_producto) VALUES (4, 6, 2);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (4, 5);
-INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (4, 7);
+INSERT INTO detalle_pedido (id_pedido, id_producto, unidades_producto) VALUES (4, 7, 2);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (4, 3);
-INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (4, 6);
-INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (4, 5);
-INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (4, 7);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (5, 2);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (5, 5);
 INSERT INTO detalle_pedido (id_pedido, id_producto) VALUES (5, 7);
@@ -171,45 +153,32 @@ INSERT INTO categoria VALUES (1,'Pizzas especiales');
 INSERT INTO categoria (nombre) VALUES ('Pizzas clásicas');
 INSERT INTO categoria (nombre) VALUES ('Pizzas vegetarianas');
 
-CREATE TABLE categoria_producto_pizza (
-    id_pizza INT UNSIGNED PRIMARY KEY,
-    id_categoria INT UNSIGNED NOT NULL,
-    FOREIGN KEY(id_pizza) REFERENCES producto(id),
-    FOREIGN KEY(id_categoria) REFERENCES categoria(id)
-);
+/* Categoria producto pizza */
+INSERT INTO categoria_producto_pizza VALUES (2, 1);
+INSERT INTO categoria_producto_pizza VALUES (1, 4);
+INSERT INTO categoria_producto_pizza VALUES (2, 5);
+INSERT INTO categoria_producto_pizza VALUES (2, 6);
 
-CREATE TABLE tienda (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    direccion VARCHAR(50) NOT NULL,
-    codigo_postal VARCHAR(5) NOT NULL,
-    localidad VARCHAR(25) NOT NULL,
-    provincia VARCHAR(25) NOT NULL
-);
 
-CREATE TABLE empleado (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(25) NOT NULL,
-    apellidos VARCHAR(50) NOT NULL,
-    NIF VARCHAR(10) NOT NULL,
-    telefono VARCHAR(9), 
-    puesto ENUM('repartidor', 'cocinero') NOT NULL,
-    id_tienda INT UNSIGNED NOT NULL
-);
+/* Tienda */
+INSERT INTO tienda (id, direccion, codigo_postal, localidad, provincia) VALUES (1, 'Plaza Cataluña 4', '08002', 'Barcelona', 'Barcelona');
+INSERT INTO tienda (direccion, codigo_postal, localidad, provincia) VALUES ('Plaza españa 2', '08045', 'Montcada', 'Barcelona');                                                                              
 
-CREATE TABLE detalle_empleados_tienda (
-    id_tienda INT UNSIGNED PRIMARY KEY,
-    id_empleado INT UNSIGNED NOT NULL,
-    FOREIGN KEY(id_tienda) REFERENCES tienda(id),
-    FOREIGN KEY(id_empleado) REFERENCES empleado(id)
-);
+/* Empleado */
 
-CREATE TABLE pedido_a_domicilio (
-    id_pedido INT UNSIGNED PRIMARY KEY,
-    id_empleado INT UNSIGNED NOT NULL,
-    fecha_hora_entrega DATETIME DEFAULT NOW() NOT NULL
-    FOREIGN KEY(id_pedido) REFERENCES pedido(id),
-    FOREIGN KEY(id_empleado) REFERENCES empleado(id)
-);
+INSERT INTO empleado (id, nombre, apellidos, NIF, Telefono, puesto, id_tienda) VALUES (1, 'Antonia', 'Marín Set', '38166597K', '968698745', 'repartidor', 1);
+INSERT INTO empleado (id, nombre, apellidos, NIF, Telefono, puesto, id_tienda) VALUES (2, 'Jose', 'Rodriguez del Haro', '98566234H', '656982564', 'cocinero', 1);
+INSERT INTO empleado (id, nombre, apellidos, NIF, Telefono, puesto, id_tienda) VALUES (3, 'Pepe', 'García Peréz', '25633458N', '968698745', 'cocinero', 2);
+INSERT INTO empleado (id, nombre, apellidos, NIF, Telefono, puesto, id_tienda) VALUES (4, 'Alef', 'Shamli Reo', 'C56778977', '968698745', 'repartidor', 1);
+INSERT INTO empleado (id, nombre, apellidos, NIF, Telefono, puesto, id_tienda) VALUES (5, 'Matt', 'Moreno García', 'h89874586', '968698745', 'repartidor', 1);
+
+/* Pedido a domicilio*/ 
+INSERT INTO pedido_a_domicilio (id_pedido, id_empleado, fecha_hora_entrega) VALUES (1, 3, '2022-12-12 13:10:00');
+
+/* Cuenta del pedido 1 */
+
+
+
 
 
 
---
+++ pruebas/SQL/schema_pizzeria_new.sql
@@ -0,0 +1,179 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema pizzeria
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema pizzeria
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `pizzeria` DEFAULT CHARACTER SET utf8mb4 ;
+USE `pizzeria` ;
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`categoria`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`categoria` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`producto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`producto` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(50) NOT NULL,
+  `descripcion` VARCHAR(225) NOT NULL,
+  `img` VARCHAR(225) NOT NULL DEFAULT 'imagen_no_disponible.jpg',
+  `precio` FLOAT(6,2) NOT NULL,
+  `tipo` ENUM('pizza', 'hamburguesa', 'bebida') NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 12
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`categoria_producto_pizza`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`categoria_producto_pizza` (
+  `id_categoria` INT(10) UNSIGNED NULL DEFAULT NULL,
+  `id_pizza` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id_pizza`),
+  INDEX `id_categoria` (`id_categoria` ASC) VISIBLE,
+  CONSTRAINT `categoria_producto_pizza_ibfk_1`
+    FOREIGN KEY (`id_pizza`)
+    REFERENCES `pizzeria`.`producto` (`id`),
+  CONSTRAINT `categoria_producto_pizza_ibfk_2`
+    FOREIGN KEY (`id_categoria`)
+    REFERENCES `pizzeria`.`categoria` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`cliente` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(25) NOT NULL,
+  `apellidos` VARCHAR(50) NOT NULL,
+  `direccion` VARCHAR(50) NOT NULL,
+  `codigo_postal` VARCHAR(5) NOT NULL,
+  `localidad` VARCHAR(25) NOT NULL,
+  `provincia` VARCHAR(25) NOT NULL,
+  `telefono` INT(9) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `telefono` (`telefono` ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 14
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`pedido`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`pedido` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `fecha_hora` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `id_cliente` INT(10) UNSIGNED NOT NULL,
+  `tipo` ENUM('local', 'domicilio') NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_cliente` (`id_cliente` ASC) VISIBLE,
+  CONSTRAINT `pedido_ibfk_1`
+    FOREIGN KEY (`id_cliente`)
+    REFERENCES `pizzeria`.`cliente` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`detalle_pedido`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`detalle_pedido` (
+  `id_pedido` INT(10) UNSIGNED NOT NULL,
+  `id_producto` INT(10) UNSIGNED NOT NULL,
+  `unidades_producto` INT(10) UNSIGNED NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id_pedido`, `id_producto`),
+  INDEX `id_producto` (`id_producto` ASC) VISIBLE,
+  CONSTRAINT `detalle_pedido_ibfk_1`
+    FOREIGN KEY (`id_pedido`)
+    REFERENCES `pizzeria`.`pedido` (`id`),
+  CONSTRAINT `detalle_pedido_ibfk_2`
+    FOREIGN KEY (`id_producto`)
+    REFERENCES `pizzeria`.`producto` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`tienda`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`tienda` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `direccion` VARCHAR(50) NOT NULL,
+  `codigo_postal` VARCHAR(5) NOT NULL,
+  `localidad` VARCHAR(25) NOT NULL,
+  `provincia` VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`empleado`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`empleado` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(25) NOT NULL,
+  `apellidos` VARCHAR(50) NOT NULL,
+  `NIF` VARCHAR(10) NOT NULL,
+  `telefono` VARCHAR(9) NULL DEFAULT NULL,
+  `puesto` ENUM('repartidor', 'cocinero') NOT NULL,
+  `id_tienda` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_tienda` (`id_tienda` ASC) VISIBLE,
+  CONSTRAINT `empleado_ibfk_1`
+    FOREIGN KEY (`id_tienda`)
+    REFERENCES `pizzeria`.`tienda` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`pedido_a_domicilio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`pedido_a_domicilio` (
+  `id_pedido` INT(10) UNSIGNED NOT NULL,
+  `id_empleado` INT(10) UNSIGNED NOT NULL,
+  `fecha_hora_entrega` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  PRIMARY KEY (`id_pedido`),
+  INDEX `id_empleado` (`id_empleado` ASC) VISIBLE,
+  CONSTRAINT `pedido_a_domicilio_ibfk_1`
+    FOREIGN KEY (`id_pedido`)
+    REFERENCES `pizzeria`.`pedido` (`id`),
+  CONSTRAINT `pedido_a_domicilio_ibfk_2`
+    FOREIGN KEY (`id_empleado`)
+    REFERENCES `pizzeria`.`empleado` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
