1e10d152c7945d85941857329b0201264972ddd3
sgilcastro <susanagilcastro@gmail.com>
2022-12-14 13:07:19 +0100

    pruebas mongodb

---
+++ pruebas/MongoDB/pruebas.js
@@ -0,0 +1,15 @@
+{
+    "nombre": "laptop",
+    "precio": 40.2,
+    "active": false,
+    "fecha_creac": new Date("12/12/99"),
+    "somedata": [1, "a", []]
+    "fabricante": {
+        "nombre": "dell"
+        "version": "xps",
+        "localizacion": {
+            "city": "usa",
+            "direccion": "ajsjaskbdjab"
+        }
+    }
+}
