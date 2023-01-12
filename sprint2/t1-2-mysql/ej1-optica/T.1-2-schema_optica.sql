3046d1e270e0305e75f96098b3a07d12a01b44e6
sgilcastro <susanagilcastro@gmail.com>
2023-01-12 12:54:48 +0100

    Más correcciones Sprint2 t1-2 mysql optica

--- sprint2/t1-2-mysql/ej1-optica/T.1-2-schema_optica.sql
+++ sprint2/t1-2-mysql/ej1-optica/T.1-2-schema_optica.sql
@@ -34,8 +34,8 @@ CREATE TABLE gafa (
     color_montura VARCHAR(25),
 	color_cristal_od VARCHAR(25),
     color_cristal_oi VARCHAR(25),
-    precio FLOAT(6,2) NOT NULL
-    fecha_venta_max DATE NOT NULL,
+    precio FLOAT(6,2) NOT NULL,
+    fecha_venta_max DATE,
     FOREIGN KEY(id_marca) REFERENCES marca(id)
 );
 
@@ -46,7 +46,7 @@ CREATE TABLE cliente (
     telefono VARCHAR(9)  NOT NULL,
     email VARCHAR(50) NOT NULL,
     fecha_registro DATE NOT NULL,
-    id_cliente_recomienda INT,
+    id_cliente_recomienda INT UNSIGNED,
     FOREIGN KEY(id_cliente_recomienda) REFERENCES cliente(id)
 
 );
@@ -69,20 +69,23 @@ CREATE TABLE venta_de_gafa (
 
 /* Proveedor */
 INSERT INTO proveedor VALUES (1, 'Slatic', 'c/Pepe', '60A', NULL, NULL, 'Barcelona', '08016', 'España','934836765', NULL, 'G38155745');
-INSERT INTO proveedor VALUES (1, 'Gafas de verdad', 'c/esa', '34', NULL, NULL, 'Madrid', '80403', 'España','914555555', NULL, 'h44444444');
+INSERT INTO proveedor VALUES (2, 'Gafas de verdad', 'c/esa', '34', NULL, NULL, 'Madrid', '80403', 'España','914555555', NULL, 'h44444444');
 
 /* Marca */
 INSERT INTO marca VALUES (1,'Ray-Ban',1);
 INSERT INTO marca VALUES (2,'Oakley',1);
 INSERT INTO marca VALUES (3,'Gucci',2);
 /* Gafa */
-INSERT INTO gafa VALUES (1, 1, 'Portaite', 9.75, 0.25, 'pasta','Negro', NULL, NULL, 98.99);
-INSERT INTO gafa (id_marca, nombre, graduacion_od, graduacion_oi, tipo, color_montura, color_cristal_od, color_cristal_oi, precio,'2022/12/31') VALUES (1, 'Gafas de sol Ray-Ban', 2.75, 2.75, 'pasta', 'negro', 'azul', 'azul', 125.00);
-INSERT INTO gafa (id_marca, nombre, graduacion_od, graduacion_oi, tipo, color_montura, color_cristal_od, color_cristal_oi, precio, '2022/06/15') VALUES (2, 'Gafas de sol Oakley', 3.00, 3.00, 'flotante', 'verde', 'verde', 'verde', 150.00);
-INSERT INTO gafa (id_marca, nombre, graduacion_od, graduacion_oi, tipo, color_montura, color_cristal_od, color_cristal_oi, precio) VALUES (3, 'Gafas de sol Gucci', 2.50, 2.50, 'metálica', 'dorado', 'transparente', 'transparente', 200.00);
+INSERT INTO gafa VALUES (1, 1, 'Portaite', 9.75, 0.25, 'pasta','Negro', NULL, NULL, 98.99,'2022/12/31');
+INSERT INTO gafa (id_marca, nombre, graduacion_od, graduacion_oi, tipo, color_montura, color_cristal_od, color_cristal_oi, precio,fecha_venta_max)
+    VALUES (1, 'Gafas de sol Ray-Ban', 2.75, 2.75, 'pasta', 'negro', 'azul', 'azul', 125.00,'2022/06/15');
+INSERT INTO gafa (id_marca, nombre, graduacion_od, graduacion_oi, tipo, color_montura, color_cristal_od, color_cristal_oi, precio) 
+    VALUES (2, 'Gafas de sol Oakley', 3.00, 3.00, 'flotante', 'verde', 'verde', 'verde', 150.00);
+INSERT INTO gafa (id_marca, nombre, graduacion_od, graduacion_oi, tipo, color_montura, color_cristal_od, color_cristal_oi, precio) 
+    VALUES (3, 'Gafas de sol Gucci', 2.50, 2.50, 'metálica', 'dorado', 'transparente', 'transparente', 200.00);
 
 /* Cliente */
-INSERT INTO cliente VALUES (1, 'Susana Gil', 'c/Escòcia, 60, 1º 1ª', '649826075', 'susanagilcastro@gmail.com', '2021/12/08');
+INSERT INTO cliente VALUES (1, 'Susana Gil', 'c/Escòcia, 60, 1º 1ª', '649826075', 'susanagilcastro@gmail.com', '2021/12/08', NULL);
 INSERT INTO cliente VALUES (2, 'Pepe Martínez', 'Rambla Nova, 42, 6º', '932658956', 'ppp@gmail.com', '2022/01/09',1);
 
 /* Empleado */
@@ -110,8 +113,7 @@ WHERE em.id = 1 AND (vdg.fecha_venta BETWEEN '2022/01/01' AND '2022/12/31');
 
 -- Llista els diferents proveïdors que han subministrat ulleres venudes amb èxit per l'òptica.
 SELECT pr.nombre 'Proveedor', ga.nombre 'Gafa', vdg.fecha_venta, ga.fecha_venta_max
-FROM proveedor pr INNER JOIN politica_de_compra pdc ON pr.id = pdc.id_proveedor
-INNER JOIN marca ma ON pdc.id_marca = ma.id
+FROM proveedor pr INNER JOIN marca ma ON pr.id = ma.id_proveedor
 INNER JOIN gafa ga ON ma.id = ga.id_marca
 INNER JOIN venta_de_gafa vdg ON ga.id = vdg.id_gafa
 WHERE vdg.fecha_venta < ga.fecha_venta_max;
--- sprint2/t1-2-mysql/ej1-optica/schema_optica_new.sql
+++ sprint2/t1-2-mysql/ej1-optica/schema_optica_new.sql
@@ -1,186 +1,290 @@
--- MySQL Workbench Forward Engineering
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-01-2023 a las 12:49:19
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.1.12
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema optica
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema optica
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `optica` DEFAULT CHARACTER SET utf8mb4 ;
-USE `optica` ;
-
--- -----------------------------------------------------
--- Table `optica`.`cliente`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `optica`.`cliente` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(25) NOT NULL,
-  `direccion` VARCHAR(50) NOT NULL,
-  `telefono` VARCHAR(9) NOT NULL,
-  `email` VARCHAR(50) NOT NULL,
-  `fecha_registro` DATE NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8mb4;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `optica`.`cliente_recomienda_otro_cliente`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `optica`.`cliente_recomienda_otro_cliente` (
-  `id_cliente_recomienda` INT(10) UNSIGNED NOT NULL,
-  `id_cliente_recomendado` INT(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id_cliente_recomienda`, `id_cliente_recomendado`),
-  INDEX `id_cliente_recomendado` (`id_cliente_recomendado` ASC) VISIBLE,
-  CONSTRAINT `cliente_recomienda_otro_cliente_ibfk_1`
-    FOREIGN KEY (`id_cliente_recomienda`)
-    REFERENCES `optica`.`cliente` (`id`),
-  CONSTRAINT `cliente_recomienda_otro_cliente_ibfk_2`
-    FOREIGN KEY (`id_cliente_recomendado`)
-    REFERENCES `optica`.`cliente` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `optica`
+--
 
--- -----------------------------------------------------
--- Table `optica`.`empleado`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `optica`.`empleado` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(40) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8mb4;
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `cliente`
+--
 
--- -----------------------------------------------------
--- Table `optica`.`marca`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `optica`.`marca` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(25) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = utf8mb4;
+CREATE TABLE `cliente` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `telefono` varchar(9) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `fecha_registro` date NOT NULL,
+  `id_cliente_recomienda` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `cliente`
+--
 
--- -----------------------------------------------------
--- Table `optica`.`gafa`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `optica`.`gafa` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `id_marca` INT(10) UNSIGNED NOT NULL,
-  `nombre` VARCHAR(25) NOT NULL,
-  `graduacion_od` FLOAT(4,2) NULL DEFAULT NULL,
-  `graduacion_oi` FLOAT(4,2) NULL DEFAULT NULL,
-  `tipo` ENUM('flotante', 'pasta', 'metálica') NOT NULL,
-  `color_montura` VARCHAR(25) NULL DEFAULT NULL,
-  `color_cristal_od` VARCHAR(25) NULL DEFAULT NULL,
-  `color_cristal_oi` VARCHAR(25) NULL DEFAULT NULL,
-  `precio` FLOAT(6,2) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `id_marca` (`id_marca` ASC) VISIBLE,
-  CONSTRAINT `gafa_ibfk_1`
-    FOREIGN KEY (`id_marca`)
-    REFERENCES `optica`.`marca` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8mb4;
+INSERT INTO `cliente` (`id`, `nombre`, `direccion`, `telefono`, `email`, `fecha_registro`, `id_cliente_recomienda`) VALUES
+(1, 'Susana Gil', 'c/Escòcia, 60, 1º 1ª', '649826075', 'susanagilcastro@gmail.com', '2021-12-08', NULL),
+(2, 'Pepe Martínez', 'Rambla Nova, 42, 6º', '932658956', 'ppp@gmail.com', '2022-01-09', 1);
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `optica`.`proveedor`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `optica`.`proveedor` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(25) NOT NULL,
-  `calle` VARCHAR(50) NOT NULL,
-  `numero` VARCHAR(10) NOT NULL,
-  `piso` VARCHAR(5) NULL DEFAULT NULL,
-  `puerta` VARCHAR(5) NULL DEFAULT NULL,
-  `ciudad` VARCHAR(25) NOT NULL,
-  `codigo_postal` VARCHAR(5) NOT NULL,
-  `pais` VARCHAR(50) NOT NULL,
-  `telefono` VARCHAR(9) NOT NULL,
-  `fax` VARCHAR(9) NULL DEFAULT NULL,
-  `NIF` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8mb4;
+--
+-- Estructura de tabla para la tabla `empleado`
+--
 
+CREATE TABLE `empleado` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- -----------------------------------------------------
--- Table `optica`.`politica_de_compra`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `optica`.`politica_de_compra` (
-  `id_marca` INT(10) UNSIGNED NOT NULL,
-  `id_proveedor` INT(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id_marca`, `id_proveedor`),
-  UNIQUE INDEX `id_marca` (`id_marca` ASC) VISIBLE,
-  INDEX `id_proveedor` (`id_proveedor` ASC) VISIBLE,
-  CONSTRAINT `politica_de_compra_ibfk_1`
-    FOREIGN KEY (`id_marca`)
-    REFERENCES `optica`.`marca` (`id`),
-  CONSTRAINT `politica_de_compra_ibfk_2`
-    FOREIGN KEY (`id_proveedor`)
-    REFERENCES `optica`.`proveedor` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+--
+-- Volcado de datos para la tabla `empleado`
+--
 
+INSERT INTO `empleado` (`id`, `nombre`) VALUES
+(1, 'Maria García'),
+(2, 'Pepe Robles');
 
--- -----------------------------------------------------
--- Table `optica`.`tiempo_de_venta`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `optica`.`tiempo_de_venta` (
-  `id_gafa` INT(10) UNSIGNED NOT NULL,
-  `fecha_venta_max` DATE NOT NULL,
-  PRIMARY KEY (`id_gafa`),
-  CONSTRAINT `tiempo_de_venta_ibfk_1`
-    FOREIGN KEY (`id_gafa`)
-    REFERENCES `optica`.`gafa` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `gafa`
+--
 
--- -----------------------------------------------------
--- Table `optica`.`venta_de_gafa`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `optica`.`venta_de_gafa` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `id_gafa` INT(10) UNSIGNED NOT NULL,
-  `id_cliente` INT(10) UNSIGNED NOT NULL,
-  `id_empleado` INT(10) UNSIGNED NOT NULL,
-  `fecha_venta` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY (`id`),
-  INDEX `id_gafa` (`id_gafa` ASC) VISIBLE,
-  INDEX `id_cliente` (`id_cliente` ASC) VISIBLE,
-  INDEX `id_empleado` (`id_empleado` ASC) VISIBLE,
-  CONSTRAINT `venta_de_gafa_ibfk_1`
-    FOREIGN KEY (`id_gafa`)
-    REFERENCES `optica`.`gafa` (`id`),
-  CONSTRAINT `venta_de_gafa_ibfk_2`
-    FOREIGN KEY (`id_cliente`)
-    REFERENCES `optica`.`cliente` (`id`),
-  CONSTRAINT `venta_de_gafa_ibfk_3`
-    FOREIGN KEY (`id_empleado`)
-    REFERENCES `optica`.`empleado` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = utf8mb4;
+CREATE TABLE `gafa` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_marca` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `graduacion_od` float(4,2) DEFAULT NULL,
+  `graduacion_oi` float(4,2) DEFAULT NULL,
+  `tipo` enum('flotante','pasta','metálica') NOT NULL,
+  `color_montura` varchar(25) DEFAULT NULL,
+  `color_cristal_od` varchar(25) DEFAULT NULL,
+  `color_cristal_oi` varchar(25) DEFAULT NULL,
+  `precio` float(6,2) NOT NULL,
+  `fecha_venta_max` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `gafa`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+INSERT INTO `gafa` (`id`, `id_marca`, `nombre`, `graduacion_od`, `graduacion_oi`, `tipo`, `color_montura`, `color_cristal_od`, `color_cristal_oi`, `precio`, `fecha_venta_max`) VALUES
+(1, 1, 'Portaite', 9.75, 0.25, 'pasta', 'Negro', NULL, NULL, 98.99, '2022-12-31'),
+(2, 1, 'Gafas de sol Ray-Ban', 2.75, 2.75, 'pasta', 'negro', 'azul', 'azul', 125.00, '2022-06-15'),
+(3, 2, 'Gafas de sol Oakley', 3.00, 3.00, 'flotante', 'verde', 'verde', 'verde', 150.00, NULL),
+(4, 3, 'Gafas de sol Gucci', 2.50, 2.50, 'metálica', 'dorado', 'transparente', 'transparente', 200.00, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `marca`
+--
+
+CREATE TABLE `marca` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `id_proveedor` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `marca`
+--
+
+INSERT INTO `marca` (`id`, `nombre`, `id_proveedor`) VALUES
+(1, 'Ray-Ban', 1),
+(2, 'Oakley', 1),
+(3, 'Gucci', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `calle` varchar(50) NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `piso` varchar(5) DEFAULT NULL,
+  `puerta` varchar(5) DEFAULT NULL,
+  `ciudad` varchar(25) NOT NULL,
+  `codigo_postal` varchar(5) NOT NULL,
+  `pais` varchar(50) NOT NULL,
+  `telefono` varchar(9) NOT NULL,
+  `fax` varchar(9) DEFAULT NULL,
+  `NIF` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`id`, `nombre`, `calle`, `numero`, `piso`, `puerta`, `ciudad`, `codigo_postal`, `pais`, `telefono`, `fax`, `NIF`) VALUES
+(1, 'Slatic', 'c/Pepe', '60A', NULL, NULL, 'Barcelona', '08016', 'España', '934836765', NULL, 'G38155745'),
+(2, 'Gafas de verdad', 'c/esa', '34', NULL, NULL, 'Madrid', '80403', 'España', '914555555', NULL, 'h44444444');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta_de_gafa`
+--
+
+CREATE TABLE `venta_de_gafa` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_gafa` int(10) UNSIGNED NOT NULL,
+  `id_cliente` int(10) UNSIGNED NOT NULL,
+  `id_empleado` int(10) UNSIGNED NOT NULL,
+  `fecha_venta` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `venta_de_gafa`
+--
+
+INSERT INTO `venta_de_gafa` (`id`, `id_gafa`, `id_cliente`, `id_empleado`, `fecha_venta`) VALUES
+(1, 1, 2, 1, '2022-11-20'),
+(2, 2, 2, 1, '2022-06-10'),
+(3, 4, 1, 1, '2022-11-20'),
+(4, 2, 1, 2, '2022-11-20');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_cliente_recomienda` (`id_cliente_recomienda`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `gafa`
+--
+ALTER TABLE `gafa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_marca` (`id_marca`);
+
+--
+-- Indices de la tabla `marca`
+--
+ALTER TABLE `marca`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_proveedor` (`id_proveedor`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `venta_de_gafa`
+--
+ALTER TABLE `venta_de_gafa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_gafa` (`id_gafa`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `id_empleado` (`id_empleado`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `gafa`
+--
+ALTER TABLE `gafa`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `marca`
+--
+ALTER TABLE `marca`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `venta_de_gafa`
+--
+ALTER TABLE `venta_de_gafa`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`id_cliente_recomienda`) REFERENCES `cliente` (`id`);
+
+--
+-- Filtros para la tabla `gafa`
+--
+ALTER TABLE `gafa`
+  ADD CONSTRAINT `gafa_ibfk_1` FOREIGN KEY (`id_marca`) REFERENCES `marca` (`id`);
+
+--
+-- Filtros para la tabla `marca`
+--
+ALTER TABLE `marca`
+  ADD CONSTRAINT `marca_ibfk_1` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`id`);
+
+--
+-- Filtros para la tabla `venta_de_gafa`
+--
+ALTER TABLE `venta_de_gafa`
+  ADD CONSTRAINT `venta_de_gafa_ibfk_1` FOREIGN KEY (`id_gafa`) REFERENCES `gafa` (`id`),
+  ADD CONSTRAINT `venta_de_gafa_ibfk_2` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`),
+  ADD CONSTRAINT `venta_de_gafa_ibfk_3` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
