4df8c080c82fb14476d6f833ae8bcd80c0411d4e
sgilcastro <susanagilcastro@gmail.com>
2023-02-01 12:42:56 +0100

    pruebas crear jsn con php

---
+++ pruebas/developers/phpJson/convertir.php
@@ -0,0 +1,54 @@
+<?php
+$people_info = [
+    "customers" => [
+        ["name" => "Andrew", "score" => 62.5],
+        ["name" => "Adam", "score" => 65.0],
+        ["name" => "Sajal", "score" => 72.2],
+        ["name" => "Monty", "score" => 57.8]
+    ]
+];
+echo json_encode($people_info);
+/* 
+{"customers":[{"name":"Andrew","score":62.5},{"name":"Adam","score":65},{"name":"Sajal","score":72.2},{"name":"Monty","score":57.8}]} 
+*/
+?>
+
+<br>
+<br>
+<br>
+
+<?php
+$people_info = [
+    "customers" => [
+        ["name" => "Andrew", "score" => 62.5],
+        ["name" => "Adam", "score" => 65.0],
+        ["name" => "Sajal", "score" => 72.2],
+        ["name" => "Monty", "score" => 57.8]
+    ]
+];
+// Agregamos banderas (constants) para obtener la cadena JSON en el formato deseado. https://www.php.net/manual/en/json.constants.php
+
+echo json_encode($people_info, JSON_PRETTY_PRINT|JSON_PRESERVE_ZERO_FRACTION);
+/* 
+{ 
+"customers": [ 
+{ 
+"name": "Andrew", 
+"score": 62.5 
+}, 
+{ 
+"name": "Adam", 
+"score": 65.0 
+}, 
+{ 
+"name": "Sajal", 
+"score": 72.2 
+}, 
+{ 
+"name": "Monty", 
+"score": 57.8 
+} 
+] 
+} 
+*/
+?>
---
+++ pruebas/developers/phpJson/crearJson.php
@@ -0,0 +1,33 @@
+<?php 
+//file_get_contents($pathOfFile, $info, $customContext, $mode);
+
+// data strored in array
+$array = Array (
+    "0" => Array (
+        "id" => "01",
+        "name" => "Olivia Mason",
+        "designation" => "System Architect"
+    ),
+    "1" => Array (
+        "id" => "02",
+        "name" => "Jennifer Laurence",
+        "designation" => "Senior Programmer"
+    ),
+    "2" => Array (
+        "id" => "03",
+        "name" => "Pepita Oliver",
+        "designation" => "Office Manager"
+    ),
+    
+    "3" => Array (
+        "id" => "04",
+        "name" => "NUevo Oliver",
+        "designation" => "Office"
+    )
+);
+
+// encode array to json
+$json = json_encode($array);
+$bytes = file_put_contents("myfile.json", $json); 
+echo "The number of bytes written are $bytes.";
+?>
