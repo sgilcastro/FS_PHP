72513e393f7b88443c00a5dbdf43afaa8d3ebc7e
sgilcastro <susanagilcastro@gmail.com>
2023-02-01 11:23:44 +0100

    convierto la class Shape en abstracta

--- sprint1/t4-php_i_objectes/nivel1/ej2/Shape.php
+++ sprint1/t4-php_i_objectes/nivel1/ej2/Shape.php
@@ -1,5 +1,5 @@
 <?php
-	class Shape
+	abstract class Shape
 	{
 		//Atributos de clase
 		protected $ancho;
@@ -12,27 +12,8 @@
 			$this->ancho = $ancho;
 			$this->alto = $alto;
 		}
+
+		abstract public function area();
 	}
 
-	//Clase hija
-	class Triangulo extends Shape
-	{
-		/*El área o superficie de un triángulo cualquiera es igual
-		al producto de la base por la altura dividido por dos.*/
-		public function area()
-		{
-			return $this->area = ($this->ancho * $this->alto) / 2;
-		}
-	}
-
-	//Clase hija
-	class Rectangulo extends Shape
-	{
-		/*Para calcular el área de un rectángulo multiplicamos
-		el largo por el ancho.*/
-		public function area()
-		{
-			return $this->area = $this->ancho * $this->alto;
-		}
-	}
 ?>
