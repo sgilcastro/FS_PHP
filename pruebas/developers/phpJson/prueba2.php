7150e1a29d5ce901a2185fe534dffb922269b4f3
sgilcastro <susanagilcastro@gmail.com>
2023-02-01 13:03:02 +0100

    pruebas bbdd jsn php prueba2.php

---
+++ pruebas/developers/phpJson/prueba2.php
@@ -0,0 +1,14 @@
+<?php
+
+$data = file_get_contents('myfile.json');
+$products = json_decode($data, true);
+
+foreach ($products as $product) {
+    echo '<pre>';
+    print_r($product);
+    echo '</pre>';
+}
+
+// añadir para modificar myfile.json
+?>
+
