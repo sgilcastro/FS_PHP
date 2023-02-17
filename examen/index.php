5ed902f5142e425ccadbf2f10b18d94d23c6fba3
sgilcastro <susanagilcastro@gmail.com>
2023-02-17 18:27:10 +0100

    pruebas

--- examen/index.php
+++ examen/index.php
@@ -1,20 +1,19 @@
 <?php
 
-include(Jugadores.php);
+include(Jugador.php);
 
 $jugadores = array();
 
-$jugador = new Jugadores("Susana", 20, 2.0, 70,'ala pivot');
-$jugador2 = new Jugadores("Susana", 20, 2.0, 70,'ala pivot');
-$jugador3 = new Jugadores("Susana", 20, 2.0, 70,'ala pivot');
-$jugador4 = new Jugadores("Susana", 20, 2.0, 70,'ala pivot');
-$jugador5 = new Jugadores("Susana", 20, 2.0, 70,'ala pivot');
-$jugador6 = new Jugadores("Susana", 20, 2.0, 70,'ala pivot');
+$jugador1 = new Jugador("Susana", 20, 2.0, 70,'ala pivot');
+$jugador2 = new Jugador("Susana", 20, 2.0, 70,'ala pivot');
+$jugador3 = new Jugador("Susana", 20, 2.0, 70,'ala pivot');
+$jugador4 = new Jugador("Susana", 20, 2.0, 70,'ala pivot');
+$jugador5 = new Jugador("Susana", 20, 2.0, 70,'ala pivot');
+$jugador6 = new Jugador("Susana", 20, 2.0, 70,'ala pivot');
 
-$jugadores = array($jugador1 ,$jugador2 ,$jugador2 ,$jugador2 ,$jugador2 );
+$jugadores = array($jugador1 ,$jugador2 ,$jugador3 ,$jugador4,$jugador5, $jugador6 );
 
-
-
+var_dump($jugadores);
 
 
 
